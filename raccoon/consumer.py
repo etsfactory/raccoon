bc76f6838735554ead6a1d8182a9dec9d89ce3d7
jarrizabalaga <fjarrizabalaga@ets.es>
2018-04-18 12:18:46 +0200

    Se repetía la función en el consumidor

--- raccoon/consumer.py
+++ raccoon/consumer.py
@@ -47,7 +47,6 @@ class Consumer(threading.Thread):
                 else:
                     data_to_process = data
                 self.messages = []
-                self.process_function(data_to_process)
                 result = self.process_function(data_to_process)
                 if self.reply:
                     ch.basic_publish(exchange='',
