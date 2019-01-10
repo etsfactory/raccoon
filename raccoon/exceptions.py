1bbdea59c9becbdbdddf690e38714968453214d3
Nelson Saturno <nsaturno@etsfactory.com>
2019-01-10 17:43:04 +0100

    ENH: Se capturan excepciones puntuales al procesar mensajes del Bus

--- raccoon/exceptions.py
+++ raccoon/exceptions.py
@@ -6,3 +6,7 @@ class RoutingErrorException(Exception):
     def __init__(self, mt_name):
         super().__init__()
         self.mt_name = mt_name
+
+
+class TransientException(Exception):
+    pass
