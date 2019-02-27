c7da8889da60d881227d88e31227d40aa4996314
Rafa <rparicio@ets.es>
2019-02-27 15:10:09 +0100

    [ADD] Doc updated

--- raccoon/exceptions.py
+++ raccoon/exceptions.py
@@ -1,18 +1,24 @@
 class ConnectionErrorException(Exception):
+    """Captura excepciones de conexión."""
     pass
 
 
 class RoutingErrorException(Exception):
+    """Captura excepciones cuando no hay coincidencias con la ruta."""
+
     def __init__(self, mt_name):
         super().__init__()
         self.mt_name = mt_name
 
 
 class TransientException(Exception):
+    """Captura excepciones que podrían ser puntuales y resolverse reintentando."""
     pass
 
 
 class PartialyProcessedException(Exception):
+    """Excepción generada cuando algunos de los mensajes han fallado al ser procesados."""
+
     def __init__(self, failed_messages):
         super().__init__(self, "Some messages failed while processing: {}".format(failed_messages))
         self.failed_messages = failed_messages
