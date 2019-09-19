bdf67767a3ab6714d8bc499e9e9b648210691a21
jcamacho <jcamacho@etsfactory.com>
2019-09-19 09:35:06 +0200

    BUG: ahora en caso de error se da informacion util

--- raccoon/consumer.py
+++ raccoon/consumer.py
@@ -47,15 +47,25 @@ class Consumer(threading.Thread):
         En caso de que la clase defina el atributo "prefetch_count", los mensajes se procesaran el bloque.
         En caso de error, pone un mensaje en la cola de errores.
         """
+        data = {}
         try:
             self.delivery_tags.append(method.delivery_tag)
-            data = ujson.loads(body)
-            federated = properties.headers and properties.headers.get('x-received-from')
-            if federated:
-                data.setdefault('metadata', {})['IsFederated'] = True
-            data.setdefault('metadata', {})['exchange'] = method.exchange
-            data.setdefault('metadata', {})['routing_key'] = method.routing_key
-            data.setdefault('metadata', {})['delivery_tag'] = method.delivery_tag
+            try:
+                message = ujson.loads(body)
+                data.update(message)
+            except Exception as e:
+                if body:
+                    data['message'] = body
+                raise e
+            finally:
+                data.setdefault('metadata', {})['exchange'] = method.exchange
+                data['metadata']['routing_key'] = method.routing_key
+                data['metadata']['delivery_tag'] = method.delivery_tag
+                data['metadata']['rabbit_queue'] = self.rabbit_queue_name
+                federated = properties.headers and properties.headers.get('x-received-from')
+                if federated:
+                    data['metadata']['IsFederated'] = True
+
             if self.data_ready():
                 if self.process_data_in_baches():
                     data_to_process = self.messages + [data]
@@ -88,7 +98,7 @@ class Consumer(threading.Thread):
         except TransientException as e:
             if method.redelivered:
                 # Se notifica el error y se reencola el mensaje apropiadamente
-                self._notify_application_exception(ch, method, properties, e, body)
+                self._notify_application_exception(ch, method, properties, e, data)
             for tag in self.delivery_tags[:]:
                 ch.basic_nack(delivery_tag=tag)
                 self.delivery_tags.remove(tag)
@@ -109,9 +119,9 @@ class Consumer(threading.Thread):
         except ConnectionClosed as e:
             raise e
         except Exception as e:
-            self._notify_application_exception(ch, method, properties, e, body)
+            self._notify_application_exception(ch, method, properties, e, data)
 
-    def _notify_application_exception(self, ch, method, properties, exc_value, body=None):
+    def _notify_application_exception(self, ch, method, properties, exc_value, data):
         """
         Notifica el error al hilo principal y rechaza el mensaje si es necesario
         """
@@ -120,8 +130,8 @@ class Consumer(threading.Thread):
             'trace': traceback.format_exc()
         }
 
-        if body:
-            exception.update({'body': body})
+        if data:
+            exception.update({'body': data})
         self.error_queue.put(exception)
 
         if self.reply:
